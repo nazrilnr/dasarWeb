3f08fef1648d3d450849fed4d40672d38d2f40a5
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-09-24 10:12:37 +0700

    JS05_PHP-2_Pratikum 8

---
+++ Jobsheet5_PHP02/p8.php
@@ -0,0 +1,17 @@
+<?php
+echo '<html>';
+echo '<head><title>Cara02</title></head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini : '.date('d M Y').'</p>';
+echo '</body>';
+echo '</html>';
+?>
+
+<html>
+<head>
+    <title>Cara 01</title>
+</head>
+<body>
+    <p>Tanggal Hari ini : <?php echo date("d M Y") ?></p>
+</body>
+</html>
