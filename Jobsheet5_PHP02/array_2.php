448fb1dad7796ac6f8fb878c7bac7362ad4d349c
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-09-24 08:11:21 +0700

    JS05_PHP-2_Pratikum 2

---
+++ Jobsheet5_PHP02/array_2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+<?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+
+    echo "Nama : {$Dosen['nama']} <br>";
+    echo "Domisili : {$Dosen['domisili']} <br>";
+    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
+?>
+</body>
+</html>
