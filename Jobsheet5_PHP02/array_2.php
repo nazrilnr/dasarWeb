3f88fc14bab82aa99d09dacdb3ec4cfb61a69a50
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-09-24 09:05:08 +0700

    JS05_PHP-2_Pratikum 2

--- Jobsheet5_PHP02/array_2.php
+++ Jobsheet5_PHP02/array_2.php
@@ -3,19 +3,55 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Display Data in Table</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+            font-family: Arial, sans-serif;
+        }
+        th, td {
+            padding: 12px;
+            text-align: left;
+            border: 1px solid #ddd;
+        }
+        th {
+            background-color: #f4f4f4;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+    </style>
 </head>
 <body>
+
 <?php
     $Dosen = [
-        'nama' => 'Elok Nur Hamdana',
+        'nama' => 'Nazreall',
         'domisili' => 'Malang',
-        'jenis_kelamin' => 'Perempuan'
+        'jenis_kelamin' => 'Laki Laki'
     ];
-
-    echo "Nama : {$Dosen['nama']} <br>";
-    echo "Domisili : {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
 ?>
+
+<table>
+    <tr>
+        <th>Attribute</th>
+        <th>Value</th>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td><?php echo $Dosen['nama']; ?></td>
+    </tr>
+    <tr>
+        <td>Domisili</td>
+        <td><?php echo $Dosen['domisili']; ?></td>
+    </tr>
+    <tr>
+        <td>Jenis Kelamin</td>
+        <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+    </tr>
+</table>
+
 </body>
 </html>
