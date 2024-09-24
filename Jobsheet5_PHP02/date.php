669f776365bc8c25e5b976c21ee8574d0b3bf01e
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-09-24 10:40:43 +0700

    JS05_PHP-2_Pratikum 10

---
+++ Jobsheet5_PHP02/date.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+    <h3> Date </h3>
+
+    <?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+    ?>
+</body>
+</html>
---
+++ Jobsheet5_PHP02/time.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+    <h3> Time </h3>
+
+    <?php
+    date_default_timezone_set("Asia/Jakarta");
+    echo date("h:i:sa");
+    ?>
+
+</body>
+</html>
