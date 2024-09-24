61971b6072bbd4470e7634a518b8655dd137ffd5
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-09-24 08:01:53 +0700

    JS05_PHP-2_Pratikum 1

---
+++ Jobsheet5_PHP02/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Array Terindeks</h2>
+<?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
+?>
+</body>
+</html>
