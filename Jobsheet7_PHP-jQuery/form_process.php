48c3ff1079f2c65d3e6a3d52de8e8d490597b9ed
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-10-08 09:07:32 +0700

    a

--- Jobsheet7_PHP-jQuery/form_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST["nama"];
-    $email = $_POST["email"];
-
-    echo "Nama: " . $nama . "<br>";
-    echo "Email: " . $email;
-}
-?>
