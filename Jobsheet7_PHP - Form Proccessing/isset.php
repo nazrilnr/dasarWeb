2d5fbd7861dfc41cc18c7fe94bc616f071ad67f0
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-10-08 09:06:39 +0700

    JS07_PHP-jQuery_Pratikum 1

---
+++ Jobsheet7_PHP - Form Proccessing/isset.php
@@ -0,0 +1,16 @@
+<?php
+$umur;
+if (isset($umur) && $umur >= 40) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+}
+echo "<br>";
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
