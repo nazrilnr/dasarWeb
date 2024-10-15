9ed628628d6d2f2d06ede3a2e82e0ccedb9922ae
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-10-15 08:22:18 +0700

    JS08_PHP_Pratikum 1

---
+++ Jobsheet8_PHP-FormUpload../form_upload.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="myfile">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
---
+++ Jobsheet8_PHP-FormUpload../upload.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+
+    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+        echo "File berhasil diunggah.";
+    }
+    else{
+        echo "Gagal mengunggah file.";
+    }
+}
+?>
