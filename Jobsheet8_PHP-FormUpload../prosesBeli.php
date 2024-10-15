b220985795edd11a426b6058ef216f6bc2d9b2f5
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-10-15 10:55:52 +0700

    JS08_PHP_Pratikum 7

---
+++ Jobsheet8_PHP-FormUpload../prosesBeli.php
@@ -0,0 +1,7 @@
+<?php
+    if (isset($_POST["beliNovel"]) && isset($_POST["beliBuku"])) {
+        setcookie("beliNovel", $_POST["beliNovel"]);
+        setcookie("beliBuku", $_POST["beliBuku"]);
+        header("location:keranjangBelanja.php");
+    }
+?>
