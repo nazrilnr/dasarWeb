fcfd3a1f882b63a1d9f8c6c8f4cb051b7b3ad214
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2024-11-03 16:47:06 +0700

    Pratikum 4

--- J10_Crud/proses.php
+++ J10_Crud/proses.php
@@ -2,12 +2,12 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
-$nama = $_POST['nama'];
-$jenis_kelamin = $_POST['jenis_kelamin'];
-$alamat = $_POST['alamat'];
-$no_telp = $_POST['no_telp'];
+$nama = $_POST['nama'] ?? null;
+$jenis_kelamin = $_POST['jenis_kelamin'] ?? null;
+$alamat = $_POST['alamat'] ?? null;
+$no_telp = $_POST['no_telp'] ?? null;
 
-if($aksi == 'tambah') {
+if ($aksi == 'tambah') {
     $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
     if (mysqli_query($koneksi, $query)) {
         header("Location: index.php");
@@ -15,7 +15,7 @@ if($aksi == 'tambah') {
     } else {
         echo "Gagal menambahkan data: " . mysqli_error($koneksi);
     }
-} elseif($aksi == 'ubah'){
+} elseif ($aksi == 'ubah') {
     if (isset($_POST['id'])) {
         $id = $_POST['id'];
         $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id=$id";
@@ -28,6 +28,21 @@ if($aksi == 'tambah') {
     } else {
         echo "ID tidak valid.";
     }
+} elseif ($aksi == 'hapus') {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "DELETE FROM anggota WHERE id=$id";
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+} else {
+    header("Location: index.php");
 }
 
 mysqli_close($koneksi);
